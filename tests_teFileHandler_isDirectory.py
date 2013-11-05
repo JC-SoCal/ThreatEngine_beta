9b71e9b09688c2ede10b40b06d17ad7801bcb921
JC <john.carruthers0@gmail.com>
2013-11-05 14:04:22 -0500

    changes

--- tests_teFileHandler_isDirectory.py
+++ tests_teFileHandler_isDirectory.py
@@ -25,8 +25,6 @@ def runIsTest(function, varible, negate=False):
 			print "FAIL:",str(varible), "in",str(function.__name__)
 			failCount += 1
 
-def runGetFilesTest(function, varible, negate=False):
-	pass
 
 #################################
 ## Set Variables for the tests ##
@@ -45,18 +43,6 @@ d4 = "tests\\teFileHandler\\dir2\\dir3"
 ## Create the test cases ##
 ###########################
 
-print "Positive File Test"
-runIsTest(teFileHandler.isFile, f1)
-runIsTest(teFileHandler.isFile, f2)
-runIsTest(teFileHandler.isFile, f3)
-runIsTest(teFileHandler.isFile, f4)
-
-print "Negative File Test"
-runIsTest(teFileHandler.isFile, d1, True)
-runIsTest(teFileHandler.isFile, d2, True)
-runIsTest(teFileHandler.isFile, d3, True)
-runIsTest(teFileHandler.isFile, d4, True)
-
 print "Positive Directory Test"
 runIsTest(teFileHandler.isDirectory, d1)
 runIsTest(teFileHandler.isDirectory, d2)
@@ -69,16 +55,6 @@ runIsTest(teFileHandler.isDirectory, f1, True)
 runIsTest(teFileHandler.isDirectory, f2, True)
 runIsTest(teFileHandler.isDirectory, f3, True)
 runIsTest(teFileHandler.isDirectory, f4, True)
-
-print teFileHandler.getFilesFromDirectory(d1,True)
-print teFileHandler.getFilesFromDirectory(d2,True)
-print teFileHandler.getFilesFromDirectory(d3,True)
-print teFileHandler.getFilesFromDirectory(d4,True)
-
-print teFileHandler.getFilesFromDirectory(d1)
-print teFileHandler.getFilesFromDirectory(d2)
-print teFileHandler.getFilesFromDirectory(d3)
-print teFileHandler.getFilesFromDirectory(d4)
 
 ###################
 ## Final Results ##
--- tests_teFileHandler_isFile.py
+++ tests_teFileHandler_isFile.py
@@ -25,21 +25,19 @@ def runIsTest(function, varible, negate=False):
 			print "FAIL:",str(varible), "in",str(function.__name__)
 			failCount += 1
 
-def runGetFilesTest(function, varible, negate=False):
-	pass
 
 #################################
 ## Set Variables for the tests ##
 #################################
-f1 = "tests\\teFileHandler\\file1.txt"
-f2 = "tests\\teFileHandler\\dir1\\file2.txt"
-f3 = "tests\\teFileHandler\\dir2\\file3.txt"
-f4 = "tests\\teFileHandler\\dir2\\dir3\\file4.txt"
+f1 = "testdata\\teFileHandler\\file1.txt"
+f2 = "testdata\\teFileHandler\\dir1\\file2.txt"
+f3 = "testdata\\teFileHandler\\dir2\\file3.txt"
+f4 = "testdata\\teFileHandler\\dir2\\dir3\\file4.txt"
 
-d1 = "tests\\teFileHandler"
-d2 = "tests\\teFileHandler\\dir1"
-d3 = "tests\\teFileHandler\\dir2"
-d4 = "tests\\teFileHandler\\dir2\\dir3"
+d1 = "testdata\\teFileHandler"
+d2 = "testdata\\teFileHandler\\dir1"
+d3 = "testdata\\teFileHandler\\dir2"
+d4 = "testdata\\teFileHandler\\dir2\\dir3"
 
 ###########################
 ## Create the test cases ##
@@ -56,29 +54,6 @@ runIsTest(teFileHandler.isFile, d1, True)
 runIsTest(teFileHandler.isFile, d2, True)
 runIsTest(teFileHandler.isFile, d3, True)
 runIsTest(teFileHandler.isFile, d4, True)
-
-print "Positive Directory Test"
-runIsTest(teFileHandler.isDirectory, d1)
-runIsTest(teFileHandler.isDirectory, d2)
-runIsTest(teFileHandler.isDirectory, d3)
-runIsTest(teFileHandler.isDirectory, d4)
-
-
-print "Negative Directory Test"
-runIsTest(teFileHandler.isDirectory, f1, True)
-runIsTest(teFileHandler.isDirectory, f2, True)
-runIsTest(teFileHandler.isDirectory, f3, True)
-runIsTest(teFileHandler.isDirectory, f4, True)
-
-print teFileHandler.getFilesFromDirectory(d1,True)
-print teFileHandler.getFilesFromDirectory(d2,True)
-print teFileHandler.getFilesFromDirectory(d3,True)
-print teFileHandler.getFilesFromDirectory(d4,True)
-
-print teFileHandler.getFilesFromDirectory(d1)
-print teFileHandler.getFilesFromDirectory(d2)
-print teFileHandler.getFilesFromDirectory(d3)
-print teFileHandler.getFilesFromDirectory(d4)
 
 ###################
 ## Final Results ##
